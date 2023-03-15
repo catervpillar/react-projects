232423a2a27e76bdaf8479bbf47c47e8c6e70bf0
Tommaso Catervi <56674392+catervpillar@users.noreply.github.com>
2023-03-15 14:51:44 +0000

    dummy multipage spa as playground for react-router

---
+++ 14-multi-page-spa/src/pages/Products.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const DUMMY_PRODUCTS = [
+    { id: 'p1', title: 'Product 1' },
+    { id: 'p2', title: 'Product 2' },
+    { id: 'p3', title: 'Product 3' }
+];
+
+const ProductsPage = () => {
+    return <>
+        <h1>Products</h1>
+        <p>Go to <Link to="/">the home page</Link>.</p>
+        <ul>
+            {DUMMY_PRODUCTS.map(product => (
+                <li key={product.id}><Link to={`${product.id}`}>{product.title}</Link></li>
+            ))}
+        </ul>
+    </>;
+};
+
+export default ProductsPage;
