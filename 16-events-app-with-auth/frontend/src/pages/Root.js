82dc5ad8266e9257cb3ab9418668cc4c1b047e1c
Tommaso Catervi <56674392+catervpillar@users.noreply.github.com>
2023-03-19 17:38:12 +0000

    authentication added

--- 16-events-app-with-auth/frontend/src/pages/Root.js
+++ 16-events-app-with-auth/frontend/src/pages/Root.js
@@ -1,12 +1,34 @@
+import { useEffect } from 'react';
 import {
   Outlet,
-  // useNavigation
+  // useNavigation,
+  useLoaderData,
+  useSubmit
 } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
   // const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post' });
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
